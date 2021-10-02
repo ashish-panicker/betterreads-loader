1a7ec06aae8d4287ba55a2f5fb8c46a17839a85b
ashish-panicker <ashish.s.panicker@outlook.com>
2021-10-02 09:18:43 +0530

    init

---
+++ src/main/java/com/easyskillup/betterreadsdataloader/BetterreadsDataloaderApplication.java
@@ -0,0 +1,13 @@
+package com.easyskillup.betterreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterreadsDataloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsDataloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyskillup/betterreadsdataloader/BetterreadsDataloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyskillup.betterreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterreadsDataloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
